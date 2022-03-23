da3b61bdb27842c3c710bc0d5592e0b6d0396eae
Kaiwangke <qqtenxun1234@gmail.com>
2022-03-23 17:50:48 -0500

    chord

--- Node.py
+++ Node.py
@@ -1,48 +1,68 @@
-# author: Pedro Garcia Lopez, PhD
-
 import random
 
-k = 6
+k = 5
 MAX = 2**k
 
+class NodeHDT{
+    def makeConnection{
+        # thrift.....
+        #############
+        
+    }
+    def run{
 
-def decr(value,size):
-    if size <= value:
-        return value - size
-    else:
-        return MAX-(size-value)
+    }
+    def considerInput{
+
+    }
+    def getWord{
+
+    }
+    def lookupkey{
+
+    }
+    def Insert{
+        # 1. try inserting key
+        # 2. inserting key
+
+    }
+}
+
+class Node{
+    def decr(value,size):
+        if size <= value:
+            return value - size
+        else:
+            return MAX-(size-value)
         
 
-def between(value,init,end):
-    if init == end:
-        return True
-    elif init > end :
-        shift = MAX - init
-        init = 0
-        end = (end +shift)%MAX
-        value = (value + shift)%MAX
-    return init < value < end
+    def between(value,init,end):
+        if init == end:
+            return True
+        elif init > end :
+            shift = MAX - init
+            init = 0
+            end = (end +shift)%MAX
+            value = (value + shift)%MAX
+        return init < value < end
 
-def Ebetween(value,init,end):
-    if value == init:
-        return True
-    else:
-        return between(value,init,end)
+    def Ebetween(value,init,end):
+        if value == init:
+            return True
+        else:
+            return between(value,init,end)
 
-def betweenE(value,init,end):
-    if value == end:
-        return True
-    else:
-        return between(value,init,end)
-
-
-class Node:
-    def __init__(self,id):
-        self.id = id
-        self.finger = {}
-        self.start = {}
-        for i in range(k):
-            self.start[i] = (self.id+(2**i)) % (2**k)
+    def betweenE(value,init,end):
+        if value == end:
+            return True
+        else:
+            return between(value,init,end)
+        def __init__(self,id):
+            self.id = id
+            self.finger = {}
+            self.start = {}
+            for i in range(k):
+                self.start[i] = (self.id+(2**i)) % (2**k)
 
     def successor(self):
         return self.finger[0]
@@ -118,91 +138,34 @@ class Node:
         self.finger[0] = succ
         
 
-def hash(line):
-    import sha
-    key=long(sha.new(line).hexdigest(),16)
-    return key
-    
+    def hash(line):
+        import sha
+        key=long(sha.new(line).hexdigest(),16)
+        return key
+        
 
-def id():
-    return long(random.uniform(0,2**k))
+    def id():
+        return long(random.uniform(0,2**k))
 
 
-def printNodes(node):
-    print ' Ring nodes:'
-    end = node
-    print node.id
-    while end != node.successor():
-        node = node.successor()
+    def printNodes(node):
+        print ' Ring nodes:'
+        end = node
         print node.id
-    print '-----------'
+        while end != node.successor():
+            node = node.successor()
+            print node.id
+        print '-----------'
 
-def showFinger(node):
-    print 'Finger table of node ' + str(node.id)
-    print 'start:node'
-    for i in range(k):
-        print str(node.start[i]) +' : ' +str(node.finger[i].id)  
-    print '-----------'
-
-
-# ----------------------The next functions are related to Pajek file generation ---------------------
-
-def node2graph(node):
-    graph = {}
-    addNode(graph,node)
-    current = node.successor()
-    while current!=node:    
-        graph[current.id] = []
-        addNode(graph,current)
-        current = current.successor()
-    return graph
-
-def addNode(graph,node):
-    graph[node.id] = []
-    for n in node.finger.values():
-        graph[node.id].append(n.id)
-
-def toPajek(node, filename):
-    graph = node2graph(node)
-    f = open(filename,'w')
-    size = len(graph)
-    f.write('*Vertices '+str(size)+'\n')
-    for i in range(size):
-        f.write(' '+str(i+1)+' "'+str(graph.keys()[i])+'"\n')
-    f.write('*Arcs\n')
-    for i in graph:
-        for conn in graph[i]:
-            f.write(' '+str(graph.keys().index(i)+1)+' '+str(graph.keys().index(conn)+1)+' 1\n')
-    f.close()
+    def showFinger(node):
+        print 'Finger table of node ' + str(node.id)
+        print 'start:node'
+        for i in range(k):
+            print str(node.start[i]) +' : ' +str(node.finger[i].id)  
+        print '-----------'
 
 
-def dec2bin2(dec,top):
-    result = ''
-    remainder = dec
-    if (dec == 0):
-            for i in range(top):
-                result = result + '0'
-            return result
-    for i in range(top-1,-1,-1):
-        result = result+ str(remainder/2**i)
-        remainder = remainder % 2**i
-    return result
-
-
-def toPajek2(node, filename):
-    graph = node2graph(node)
-    f = open(filename,'w')
-    size = len(graph)
-    f.write('*Vertices '+str(size)+'\n')
-    for i in range(size):
-        id1 = dec2bin2(graph.keys()[i],k)
-        f.write(' '+str(i+1)+' "'+str(id1)+'"\n')
-    f.write('*Arcs\n')
-    for i in graph:
-        for conn in graph[i]:
-            f.write(' '+str(graph.keys().index(i)+1)+' '+str(graph.keys().index(conn)+1)+' 1\n')
-    f.close()
-
-
+}
+    
 
 
