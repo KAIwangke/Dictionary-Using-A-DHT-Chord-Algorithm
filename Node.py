8c8339d0be713b82856103201e5938de35a84ba9
KAIwangke <46043861+KAIwangke@users.noreply.github.com>
2022-03-28 18:01:54 -0500

    Update Node.py

--- Node.py
+++ Node.py
@@ -1,41 +1,32 @@
+import numbers
 import random
 
-k = 9
+
+
+k = 6
 # Assume the Number of the nodes are 9
 MAX = 2**k
+# Assume the longest steps need to take the 2^k step
 
-class NodeHDT{
-    def makeConnection{
-        # thrift.....
-        #############
-        
-    }
-    def run{
+# we need use the node's (IP + Node) change to the hash value
+# result mod MAX
 
-    }
-    def considerInput{
+# find the closet numbers
 
-    }
-    def getWord{
 
-    }
-    def lookupkey{
+import random
+import hashlib
 
-    }
-    def Insert{
-        # 1. try inserting key
-        # 2. inserting key
+k = 6
+MAX = 2**k
 
-    }
-}
-
-class Node{
+class pos:
     def decr(value,size):
         if size <= value:
             return value - size
         else:
             return MAX-(size-value)
-        
+            
 
     def between(value,init,end):
         if init == end:
@@ -47,29 +38,32 @@ class Node{
             value = (value + shift)%MAX
         return init < value < end
 
-    def Ebetween(value,init,end):
+    def atStart(value,init,end):
         if value == init:
             return True
         else:
-            return between(value,init,end)
+            return pos.between(value,init,end)
 
-    def betweenE(value,init,end):
+    def atEND(value,init,end):
         if value == end:
             return True
         else:
-            return between(value,init,end)
-        def __init__(self,id):
-            self.id = id
-            self.finger = {}
-            self.start = {}
-            for i in range(k):
-                self.start[i] = (self.id+(2**i)) % (2**k)
+            return pos.between(value,init,end)
+
+
+class Node:
+    def __init__(self,id):
+        self.id = id
+        self.finger = {}
+        self.start = {}
+        for i in range(k):
+            self.start[i] = (self.id+(2**i)) % (2**k)
 
     def successor(self):
         return self.finger[0]
     
     def find_successor(self,id):  
-        if betweenE(id,self.predecessor.id,self.id):
+        if pos.atEND(id,self.predecessor.id,self.id):
             return self
         n = self.find_predecessor(id)
         return n.successor()
@@ -78,14 +72,13 @@ class Node{
         if id == self.id:
             return self.predecessor
         n1 = self
-        while not betweenE(id,n1.id,n1.successor().id):
-
+        while not pos.atEND(id,n1.id,n1.successor().id):
             n1 = n1.closest_preceding_finger(id)
         return n1
     
     def closest_preceding_finger(self,id):
         for i in range(k-1,-1,-1):
-            if between(self.finger[i].id,self.id,id):
+            if pos.between(self.finger[i].id,self.id,id):
                 return self.finger[i]
         return self
         
@@ -106,28 +99,28 @@ class Node{
         self.successor().predecessor = self
         self.predecessor.finger[0] = self
         for i in range(k-1):
-            if Ebetween(self.start[i+1],self.id,self.finger[i].id):
+            if pos.atStart(self.start[i+1],self.id,self.finger[i].id):
                 self.finger[i+1] = self.finger[i]
             else :
                 self.finger[i+1] = n1.find_successor(self.start[i+1])
 
     def update_others(self):
         for i in range(k):
-            prev  = decr(self.id,2**i)
+            prev  = pos.decr(self.id,2**i)
             p = self.find_predecessor(prev)
             if prev == p.successor().id:
                 p = p.successor()
             p.update_finger_table(self,i)
             
     def update_finger_table(self,s,i):
-        if Ebetween(s.id,self.id,self.finger[i].id) and self.id!=s.id:
+        if pos.atStart(s.id,self.id,self.finger[i].id) and self.id!=s.id:
                 self.finger[i] = s
                 p = self.predecessor
                 p.update_finger_table(s,i)
 
     def update_others_leave(self):
         for i in range(k):
-            prev  = decr(self.id,2**i)
+            prev  = pos.decr(self.id,2**i)
             p = self.find_predecessor(prev)
             p.update_finger_table(self.successor(),i)
     # not checked 
@@ -138,36 +131,47 @@ class Node{
         
     def setSuccessor(self,succ):
         self.finger[0] = succ
-        
-
-    def hash(line):
-        import sha
-        key=long(sha.new(line).hexdigest(),16)
-        return key
-        
-
-    def id():
-        return long(random.uniform(0,2**k))
-
-
-    def printNodes(node):
-        print ' Ring nodes:'
-        end = node
-        print node.id
-        while end != node.successor():
-            node = node.successor()
-            print node.id
-        print '-----------'
-
-    def showFinger(node):
-        print 'Finger table of node ' + str(node.id)
-        print 'start:node'
-        for i in range(k):
-            print str(node.start[i]) +' : ' +str(node.finger[i].id)  
-        print '-----------'
-
-
-}
     
 
+    # def stabilization()
+        # 1. node x join and Id is between xp and xs
+        # 2. x.suc  = xs
+        # 3. xs.pre = x
+        # 4. stabilization(xp)
+        #     4.1 ?xs.pre->x
+        #     4.2 xp.suc = x
+        #     4.3 x.pre = xp
 
+
+
+
+def hash(line):
+    key=(sha.new(line).hexdigest(),16)
+    return key
+    
+
+def id():
+    return long(random.uniform(0,2**k))
+
+
+def printNodes(node):
+    print (" Ring nodes:")
+    end = node
+    print (node.id)
+    while end != node.successor():
+        node = node.successor()
+        print (node.id)
+    print ('-----------')
+
+def showFinger(node):
+    print ('Finger table of node ' + str(node.id))
+    print ('start:node')
+    for i in range(k):
+        print (str(node.start[i]) +' : ' +str(node.finger[i].id))  
+    print ('-----------')
+
+
+
+# todo figure out the readin
+# read in and 
+# generate the hash function(IP and port)
